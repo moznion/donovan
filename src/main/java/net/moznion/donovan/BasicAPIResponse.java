aff0a8752ab30286c01a4f490ea462fc7ce4c749
moznion <moznion@gmail.com>
2015-03-13 10:09:11 +0900

    Reformat

--- src/main/java/net/moznion/donovan/BasicAPIResponse.java
+++ src/main/java/net/moznion/donovan/BasicAPIResponse.java
@@ -10,38 +10,37 @@ import java.util.List;
 @EqualsAndHashCode
 @ToString
 public class BasicAPIResponse {
-	private int code;
-	private List<String> messages;
+  private int code;
+  private List<String> messages;
 
-	public BasicAPIResponse() {
-		this.code = 200;
-		this.messages = Collections.emptyList();
-	}
+  public BasicAPIResponse() {
+    this.code = 200;
+    this.messages = Collections.emptyList();
+  }
 
-	public BasicAPIResponse(int code, String message) {
-		this.code = code;
-		this.messages = Arrays.asList(message);
-	}
+  public BasicAPIResponse(int code, String message) {
+    this.code = code;
+    this.messages = Arrays.asList(message);
+  }
 
-	public BasicAPIResponse(int code, List<String> messages) {
-		this.code = code;
-		this.messages = messages;
-	}
+  public BasicAPIResponse(int code, List<String> messages) {
+    this.code = code;
+    this.messages = messages;
+  }
 
-	public int getCode() {
-		return this.code;
-	}
+  public int getCode() {
+    return this.code;
+  }
 
-	public void setCode(int code) {
-		this.code = code;
-	}
+  public void setCode(int code) {
+    this.code = code;
+  }
 
-	public List<String> getMessages() {
-		return this.messages;
-	}
+  public List<String> getMessages() {
+    return this.messages;
+  }
 
-	public void setMessages(List<String> messages) {
-		this.messages = messages;
-	}
-
+  public void setMessages(List<String> messages) {
+    this.messages = messages;
+  }
 }
